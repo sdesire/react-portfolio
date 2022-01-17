64efdfa07080799c7dd3c7da285bf0a1c5dac8e9
sdesire <sdesire@ltu.edu>
2022-01-16 20:06:27 -0500

    add components

---
+++ src/components/footer/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<footer className="footer">
+			<div>
+				<a
+					href="https://github.com/sdesire"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("../../assets/logos/github-logo.png")}
+						alt="Github"
+						className="logo"
+					></img>
+				</a>
+			</div>
+			<div>
+				<a
+					href="www.linkedin.com/in/sean-desire-222798219"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("../../assets/logos/linkedin-logo.png")}
+						alt="LinkedIn"
+						className="logo"
+					></img>
+				</a>
+			</div>
+			<div>
+				<a
+					href="https://twitter.com/SeanDesire2"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={require("../../assets/logos/twitter-logo.png")}
+						alt="Twitter"
+						className="logo"
+					></img>
+				</a>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
